c8a744bd95a05674cd242f6ff74b9802ce278358
nathan <144018979+nhuijser@users.noreply.github.com>
2023-11-28 17:34:33 +0100

    add: h2

---
+++ H2/h2.php
@@ -0,0 +1,24 @@
+<?php
+$a = "Ik heb niet gefaald";
+$b = "die niet werken";
+$c = 10000;
+
+echo $a . ". Ik heb alleen " . $c . " manieren gevonden " . $b . ".";
+
+$d = "die geen fout";
+$e = "en";
+$f = "niet.";
+
+echo "<br>Mensen " . $d . $e . " maken, werken " . $f;
+
+$g = "ry"; 
+$h = ", but";
+$i = "fail";
+
+echo "<br>T" . $g . " and " . $i . $h . " never " . $i . " to t" . $g . ".";
+
+$j = "\"omgaan";
+$k = "met";
+$l = "teleu";
+
+echo "<br>De cursus " . $j . " " . $k . " " . $l . "rstellingen\" kan vanavond helaas niet doorgaan.";
