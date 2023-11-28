428016c96e89276ef95050f00d0c881dc632e73e
nathan <144018979+nhuijser@users.noreply.github.com>
2023-11-28 21:50:21 +0100

    add: 3.2

---
+++ H3/3.2.php
@@ -0,0 +1,7 @@
+<?php
+
+$bomen = array("img_0050.jpg", "lillypilly1.jpg", "Maranchery_Biyyam_Kayal_kandal.jpg", "weeping-elm.jpg", "weeping-elm0091.jpg");
+
+foreach ($bomen as $boom) {
+    echo "<img src='bomen/" . $boom . "'>";
+}
