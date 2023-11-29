8c96ad632e639a8abe96ca21989a6f9fcdbacb74
nathan <144018979+nhuijser@users.noreply.github.com>
2023-11-29 10:59:16 +0100

    add: 5.5

---
+++ H5/5.5/5.5.php
@@ -0,0 +1,7 @@
+<body>
+<form action="resultaat.php" method="GET">
+E-mail<input type="text" name="email" value="">
+Wachtwoord<input type="password" name="wachtwoord" value="">
+	<input type="submit" name="knop" value="verstuur">
+</form>
+</body>
---
+++ H5/5.5/resultaat.php
@@ -0,0 +1,21 @@
+    <?php
+        $emailreceived = $_GET["email"];
+        $wachtwoordreceived = $_GET["wachtwoord"];
+
+        $users = array(
+            "piet@worldonline.nl" => "doetje123",
+            "klaas@carpets.nl" => "snoepje777",
+            "truushendriks@wegweg.nl" => "arkiearkie201",
+        );
+
+        $login = false;
+
+        foreach($users as $email => $wachtwoord) {
+            if($emailreceived == $email && $wachtwoordreceived == $wachtwoord) {
+                $login = true;
+                break;
+            }
+        }
+
+        echo $login ? 'true' : 'false';
+    ?>
