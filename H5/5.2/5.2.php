f324b34a687c0697f486c5dd950a20e25f9e73bf
nathan <144018979+nhuijser@users.noreply.github.com>
2023-11-29 10:23:43 +0100

    add: 5.2

---
+++ H5/5.2/5.2.php
@@ -0,0 +1,9 @@
+<body>
+<form action="resultaat.php" method="GET">
+	Inlognaam<input type="text" name="inlognaam" value="" required>
+    E-mail<input type="email" name="email" value="" required>
+    Adres<input type="adres" name="adres" value="" required>
+	Wachtwoord<input type="password" name="wachtwoord" value="" required>
+	<input type="submit" name="knop" value="verstuur">
+</form>
+</body>
---
+++ H5/5.2/resultaat.php
@@ -0,0 +1,10 @@
+<?php
+    $inlognaam = $_GET["inlognaam"];
+    $email = $_GET["email"];
+    $adres = $_GET["adres"];
+    $wachtwoord = $_GET["wachtwoord"];
+
+    echo "<br> Jouw inlognaam is: " . $inlognaam;
+    echo "<br> Jouw e-mail is: " . $email;
+    echo "<br> Jouw adres is: " . $adres;
+    echo "<br> Jouw wachtwoord is: " . $wachtwoord;
