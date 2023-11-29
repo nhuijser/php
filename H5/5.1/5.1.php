d494abfb7d62b670d02305035d7e016e00625d2e
nathan <144018979+nhuijser@users.noreply.github.com>
2023-11-29 10:23:26 +0100

    add: 5.1

---
+++ H5/5.1/5.1.php
@@ -0,0 +1,7 @@
+<body>
+<form action="resultaat.php" method="GET">
+	Inlognaam<input type="text" name="inlognaam" value="">
+	Wachtwoord<input type="password" name="wachtwoord" value="">
+	<input type="submit" name="knop" value="verstuur">
+</form>
+</body>
